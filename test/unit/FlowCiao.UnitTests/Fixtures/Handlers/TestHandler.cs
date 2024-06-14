106614ead9f336cc77fc4eb9616f4907de27e26b
Kingfish <https.mohammad@gmail.com>
2024-06-14 12:21:51 +0330

    Adds trigger handler tests

---
+++ test/unit/FlowCiao.UnitTests/Fixtures/Handlers/TestHandler.cs
@@ -0,0 +1,24 @@
+﻿using FlowCiao.Handle;
+using FlowCiao.Models;
+using FlowCiao.Models.Execution;
+using FlowCiao.Persistence.Interfaces;
+using FlowCiao.Services;
+
+namespace FlowCiao.UnitTests.Fixtures.Handlers;
+
+internal class TestHandler : FlowHandler
+{
+    public TestHandler(IFlowRepository flowRepository, FlowService flowStepManager) : base(flowRepository, flowStepManager)
+    {
+    }
+
+    public override FlowResult Handle(FlowStepContext flowStepContext)
+    {
+        return new FlowResult();
+    }
+
+    public override FlowResult RollBack(FlowStepContext flowStepContext)
+    {
+        return new FlowResult();
+    }
+}
---
+++ test/unit/FlowCiao.UnitTests/Handle/Handlers/TriggerHandlerTests.cs
@@ -0,0 +1,38 @@
+﻿using FlowCiao.Handle.Handlers;
+using FlowCiao.Models.Execution;
+using FlowCiao.Persistence.Interfaces;
+using FlowCiao.Services;
+using FlowCiao.UnitTests.Fixtures;
+using FlowCiao.UnitTests.Fixtures.Handlers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FlowCiao.UnitTests.Handle.Handlers;
+
+public class TriggerHandlerTests : IClassFixture<ServiceProviderFixture>
+{
+    private readonly IFlowRepository _flowRepository;
+    private readonly FlowService _flowService;
+    
+    public TriggerHandlerTests(ServiceProviderFixture serviceProviderFixture)
+    {
+        _flowRepository = serviceProviderFixture.ServiceProvider.GetService<IFlowRepository>();
+        _flowService = serviceProviderFixture.ServiceProvider.GetService<FlowService>();
+    }
+    
+    [Fact]
+    public void Handle_ShouldWork()
+    {
+        var testHandler = new TestHandler(_flowRepository, _flowService);
+        var handler = new TriggerHandler(_flowRepository, _flowService);
+        handler.SetNextHandler(testHandler);
+        
+        var context = new FlowStepContext
+        {
+            FlowInstanceStepDetail = new FlowInstanceStepDetail()
+        };
+
+        var result = handler.Handle(context);
+        
+        Assert.Equal("completed", result.Status);
+    }
+}
